a71c4d592bc5a24ebf159f4e92f75e1cba485ce8
Alex9517535 <Alex9517535@yahoo.com>
2025-09-08 22:50:46 -0400

    Initial commit: Vite + React + TS scaffold and MVP files

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'system-ui' }}>
+      <h1>Hello, world 👋</h1>
+      <p>If you can see this, React is mounting correctly.</p>
+    </div>
+  );
+}
